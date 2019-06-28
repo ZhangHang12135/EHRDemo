7967db8ba3293610da9aca60a4084eaf1ea8e0d3
ZhangHang12135 <z839187405@qq.com>
2019-06-28 21:30:55 +0800

    add salaryList service

--- src/main/java/com/ehr/service/SalaryListService.java
+++ src/main/java/com/ehr/service/SalaryListService.java
@@ -2,16 +2,15 @@ package com.ehr.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
 
 import com.ehr.model.SalaryList;
 import com.github.pagehelper.PageInfo;
 
-@Service
+
 public interface SalaryListService {
 	public int deleteByPrimaryKey(Integer id);
 	public SalaryList selectByPrimaryKey(Integer id);
-	public int updateByPrimaryKey(Integer id);
+	public int updateByPrimaryKey(SalaryList salaryList);
 	public List<SalaryList> selectAll();
 	PageInfo<SalaryList> pageList(Integer pageNum,Integer pageSize);
 }
